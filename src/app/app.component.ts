ca244d93d541832995ea7657394f117322ae4db7
lvctech <csjblcordero@gmail.com>
2025-07-26 16:15:59 -0400

    Adding a loading loop in the interface.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,36 +10,42 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  movies = [
-    {
-      title: 'Spider-Man: Across the Spider-Verse',
-      releaseDate: new Date(),
-      price: 12.99,
-      poster: 'assets/spiderman.jpg' // corregido
-    },
-    {
-      title: 'Moana 2',
-      releaseDate: new Date("2016-05-03"),
-      price: 20.50,
-      poster: 'assets/moana.jpg'
-    },
-    {
-      title: 'How to train your dragon',
-      releaseDate: new Date("2016-05-03"),
-      price: 25,
-      poster: 'assets/httyd.jpg'
-    },
-    {
-      title: 'Jurassic Park 3',
-      releaseDate: new Date("2016-05-03"),
-      price: 33.85,
-      poster: 'assets/jurassicpark.jpg'
-    },
-    {
-      title: "Howl's Moving Castle",
-      releaseDate: new Date("2016-05-03"),
-      price: 8.99,
-      poster: 'assets/howls_moving_castle.jpg'
-    }
-  ];
+  movies?: any[];
+
+  constructor() {
+    setTimeout(() => {
+        this.movies = [
+        {
+          title: 'Spider-Man: Across the Spider-Verse',
+          releaseDate: new Date("06-02-2023"),
+          price: 12.99,
+          poster: 'assets/spiderman.jpg' // corregido
+        },
+        {
+          title: 'Moana 2',
+          releaseDate: new Date("11-27-2024"),
+          price: 20.50,
+          poster: 'assets/moana.jpg'
+        },
+        {
+          title: 'How to train your dragon',
+          releaseDate: new Date("03-26-2010"),
+          price: 25,
+          poster: 'assets/httyd.jpg'
+        },
+        {
+          title: 'Jurassic Park 3',
+          releaseDate: new Date("07-18-2001"),
+          price: 33.85,
+          poster: 'assets/jurassicpark.jpg'
+        },
+        {
+          title: "Howl's Moving Castle",
+          releaseDate: new Date("2004-11-20"),
+          price: 8.99,
+          poster: 'assets/howls_moving_castle.jpg'
+        }
+      ];
+    }, 2000); // Simulate a delay for loading movies
+  }
 }
