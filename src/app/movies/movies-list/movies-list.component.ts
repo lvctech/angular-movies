729529d9bcda62776320d68a802520c279ab668f
lvctech <csjblcordero@gmail.com>
2025-07-30 23:09:37 -0400

    Adding functioning buttons to the website.

--- src/app/movies/movies-list/movies-list.component.ts
+++ src/app/movies/movies-list/movies-list.component.ts
@@ -10,6 +10,19 @@ import { RouterOutlet } from '@angular/router';
 })
 export class MoviesListComponent {
   @Input({required: true})
-  movies?: any[];
+  movies!: any[];
+
+  addMovie() {
+    this.movies?.push({
+      title: 'Inception',
+      releaseDate: new Date('2017-07-03'),
+      price: 50.00,
+    });
+  }
+
+  removeMovie(movie: any) {
+    let index = this.movies.findIndex((m: any) => m.title === movie.title);
+    this.movies.splice(index, 1);
+  }
 
 }
