a955134e61b74f95be1883bc127ce5395523bf43
lvctech <csjblcordero@gmail.com>
2025-08-23 23:48:05 -0400

    Creating a form.

--- src/app/genres/create-genre/create-genre.component.ts
+++ src/app/genres/create-genre/create-genre.component.ts
@@ -2,19 +2,29 @@ import { Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { Router, RouterLink } from '@angular/router';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
+
 @Component({
   selector: 'app-create-genre',
-  imports: [MatButtonModule, MatIconModule, RouterLink],
+  imports: [MatButtonModule, MatIconModule, RouterLink, ReactiveFormsModule, MatFormFieldModule, MatInputModule],
   templateUrl: './create-genre.component.html',
   styleUrl: './create-genre.component.css'
 })
 export class CreateGenreComponent {
 
   router = inject(Router);
+  private formBuilder = inject(FormBuilder);
+
+  form = this.formBuilder.group({
+    name: ''
+  });
 
   saveChanges() {
     // Logic to save the new genre
-
+    console.log(this.form.value);
     this.router.navigate(['/genres']);
   }
 }
